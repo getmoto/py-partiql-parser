86fc48776d57079c517ee3f74c54f5bf700522e9
Bert Blommers <info@bertblommers.nl>
2022-08-21 13:48:59 +0000

    Fix tests

--- tests/test_from_parser.py
+++ tests/test_from_parser.py
@@ -3,25 +3,25 @@ from py_partiql_parser._internal.parser import FromParser
 
 
 def test_split__single_source():
-    FromParser().split_clauses("x").should.equal({"x": "x"})
+    FromParser().parse("x").should.equal({"x": "x"})
 
 
 def test_split__multiple_sources():
-    FromParser().split_clauses("x,y").should.equal({"x": "x", "y": "y"})
+    FromParser().parse("x,y").should.equal({"x": "x", "y": "y"})
 
 
 def test_split__multiple_sources_with_aliases():
-    FromParser().split_clauses("x, y AS z").should.equal({"x": "x", "z": "y"})
-    FromParser().split_clauses("x AS y,z").should.equal({"y": "x", "z": "z"})
-    FromParser().split_clauses("a AS b, x AS y").should.equal({"b": "a", "y": "x"})
+    FromParser().parse("x, y AS z").should.equal({"x": "x", "z": "y"})
+    FromParser().parse("x AS y,z").should.equal({"y": "x", "z": "z"})
+    FromParser().parse("a AS b, x AS y").should.equal({"b": "a", "y": "x"})
 
 
 def test_split__single_source_with_alias():
-    FromParser().split_clauses("x AS y").should.equal({"y": "x"})
+    FromParser().parse("x AS y").should.equal({"y": "x"})
 
 
 def test_split__nonliteral_clauses():
-    FromParser().split_clauses("[1, 2, 3] AS y").should.equal({"y": "[1, 2, 3]"})
-    FromParser().split_clauses("a AS b, [1, 2, 3] AS y").should.equal(
+    FromParser().parse("[1, 2, 3] AS y").should.equal({"y": "[1, 2, 3]"})
+    FromParser().parse("a AS b, [1, 2, 3] AS y").should.equal(
         {"b": "a", "y": "[1, 2, 3]"}
     )
